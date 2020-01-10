a414fd637a37f105b322fb4edacd08dc1e96f167
jsonKK <595675516@qq.com>
2020-01-10 18:09:03 +0800

    增加变量方法测试

--- es6-webpack/src/1-variate.js
+++ es6-webpack/src/1-variate.js
@@ -1,9 +1,27 @@
 export function main(){
+
     {
-        // 拉平数组
-        let arr = [1,[2,[3,[{name:'jsonKK'}]]]];
-        console.log('拉平1个数组',arr.flat());
-        console.log('拉平2个数组',arr.flat(2));
-        console.log('拉平全部个数组',arr.flat(Infinity));
+        // 重复定义变量
+        function f(){
+            console.log('重复定义变量','this is outside');
+        }
+        (function(){
+            function f(){
+                console.log('重复定义变量','this is inside');
+            }
+            if(false){}
+            f();
+        })()
     }
+
+    {
+        //对象const
+        const foo = {};
+
+        foo.prop = 123;
+        console.log('改变const对象foo的值',foo);
+        
+    }
+    
+
 }
