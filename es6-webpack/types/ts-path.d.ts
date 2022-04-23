a390107de79bb96f791d3be7623c99bce7cba1e9
laisujie <laisujie@taqu.com>
2022-04-23 16:13:19 +0800

    增加ts配置

--- es6-webpack/types/ts-path.d.ts
+++ es6-webpack/types/ts-path.d.ts
@@ -4,5 +4,11 @@ declare module '@pages/21-fetch';
 
 declare module '@utils/index.ts';
 declare module '@demo/deep-array.ts';
-declare module '@demo/sleep.ts';
+declare module '@demo/sleep.ts'{
+  export class sleep {
+    constructor();
+    fn():void;
+    sleep():void;
+  }
+}
 
